4c3c64832497909bdd4f57da065ae97b2147a720
Aaronica123 <k.aaronmutua@gmail.com>
2025-07-27 22:08:59 +0300

    {Comm}

--- backend/controller/profile.js
+++ backend/controller/profile.js
@@ -1,20 +1,28 @@
-const prof=require("../model/register")
+const Prof = require("../model/register");
 
-const profile1=async(req,res)=>{
-    const {username}=req.query;
-    const s=await prof.find({
-          username: { $regex: `^${username}$`, $options: 'i' }
-    })
-if(s.length>0){
-    return res.status(200).json({
-        message:"Found",
-        data:s
-    })}
-    else{
-        return res.status(200).json({
-        message:"NotFound",
-        
-    })
+const profile1 = async (req, res) => {
+  const { username } = req.query;
+  try {
+    if (!username) {
+      return res.status(400).json({ message: "Username is required" });
     }
-}
-module.exports={profile1}
+    const user = await Prof.findOne({
+      username: { $regex: `^${username}$`, $options: 'i' }
+    });
+    if (user) {
+      const { firstname, lastname, username: foundUsername } = user;
+      return res.status(200).json({
+        message: "Found",
+        data: { firstname: firstname || '', lastname: lastname || '', username: foundUsername }
+      });
+    } else {
+      return res.status(404).json({ message: "NotFound" });
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+module.exports = { profile1 };
